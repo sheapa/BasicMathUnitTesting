530dd2773d645073d13d3c07446660d4daec5442
sheapa <patshea26@gmail.com>
2022-02-16 18:52:49 -0500

    Basic math unit tests complete

---
+++ CaclulationLibrary/Calculations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaclulationLibrary
+{
+    // method stubbs
+    public class Calculations
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divid(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ CalculationLibary.Tests/CalculationsTests.cs
@@ -0,0 +1,70 @@
+﻿using CaclulationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculationLibary.Tests
+{
+    public class CalculationsTests
+    {
+        [Theory]
+        [InlineData(4, 2, 6)]
+        public void AddShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations calc = new Calculations();
+
+            //Act
+            double actual = calc.Add(x, y);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4,2,2)]
+        public void SubtractShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations Calc = new Calculations();
+
+            //Act
+            double actual = Calc.Subtract(x, y);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4,2,8)]
+        public void MultiplyShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations Calc = new Calculations();
+
+            //Act
+            double actual = Calc.Multiply(x, y);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4,2,2)]
+        public void DivideShouldReturnExpectedValue(double x, double y, double expected)
+        {
+            //Arrange
+            Calculations Calc = new Calculations();
+
+            //Act
+            double actual = Calc.Divid(x, y);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
